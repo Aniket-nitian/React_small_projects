a395be945ea72586551863bf56e3782cf4467a61
Aniket-nitian <anik807621@gmail.com>
2025-05-23 15:47:02 +0530

    React router learn

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import Header from "./components/Header/header";
+import Home from "./components/Home/Home";
+import Footer from "./components/Footer/Footer";
+
+function App() {
+  return (
+    <>
+      {/* <Home />
+      <Footer /> */}
+    </>
+  );
+}
+
+export default App;
